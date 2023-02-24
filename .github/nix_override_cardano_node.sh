6974026132c3e69891174bc2d2bccc31910bacd1
Jean-Baptiste Giraudeau <jb@giraudeau.info>
2023-02-24 20:42:08 +0100

    Add support for NODE_PATH

--- .github/nix_override_cardano_node.sh
+++ .github/nix_override_cardano_node.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
 node_override() {
-  local ref
-  # if argument is provided:
+  local node_override
+  github_prefix="github:input-output-hk/cardano-node"
+  # If argument is provided:
   if [ -n "${1:-""}" ]; then
-    ref="/$1"
+    node_override="$github_prefix/$1"
   # else use specified branch and/or revision:
   elif [ -n "${NODE_REV:-""}" ]; then
-    ref="/$NODE_REV"
+    node_override="$github_prefix/$NODE_REV"
+  elif [ -n "${NODE_BRANCH:-""}" ]; then
+    node_override="$github_prefix/$NODE_BRANCH"
+  elif [ -n "${NODE_PATH:-""}" ]; then
+    node_override="path:$NODE_PATH"
   else
-    # otherwise update to latest from default branch
-    ref=""
+    #otherwise update to latest from default branch.
+    node_override=$github_prefix
   fi
-  echo --override-input cardano-node "github:input-output-hk/cardano-node$ref" --recreate-lock-file
+  echo --override-input cardano-node $node_override --recreate-lock-file
 }
